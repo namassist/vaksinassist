5a695b74f30fcbc49031af3ce3a6dd3ed6de0ae2
namassist <chairulanmm@gmail.com>
2021-09-12 20:30:23 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center bg-blue-500 font-poppins">
+      <h1 className="text-gray-300 text-4xl font-semibold">Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
