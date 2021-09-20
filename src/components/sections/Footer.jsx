0df38182250deb7bb7058f4ce35998ad1e0f6e46
namassist <chairulanmm@gmail.com>
2021-09-20 23:14:35 +0700

    update

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
     <footer className="w-full h-32 py-5 border-t-2 bg-fourth-color border-gray-200  items-center flex text-sm text-primary">
       <Container>
         <div className="flex flex-col lg:flex-row justify-between items-center">
-          <div className="w-4/12">
+          <div className="w-full lg:w-4/12">
             <img src={Logo} alt="Logo" />
           </div>
-          <div className="w-6/12 flex justify-end tracking-wide font-semibold">
+          <div className="w-full lg:w-6/12 flex lg:justify-end tracking-wide font-semibold">
             &copy; Copyright 2021 | Built by&nbsp;
             <a
               className="animate-pulse transform hover:scale-105 hover:text-sixth transition-all duration-150 "
